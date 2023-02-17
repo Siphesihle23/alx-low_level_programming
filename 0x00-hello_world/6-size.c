c9d23c5d8b5f55f3dde1db919901bc57ced1763b
Siphesihle <sihles406@gmail.com>
2023-02-16 18:16:06 -0800

    size of type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the size of various types on the computer
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("Size of char: %1 byte(s)/n", sizeof(char));
+	printf("Size of int: %1 byte(s)/n", sizeof(int));
+	printf("Size of long int: %1 byte(s)/n", sizeof(long int));
+	printf("Size of long long int: %1 byte(s)/n", sizeof(long long int));
+	printf("Size of float: %1 byte(s)/n", sizeof(float));
+	return (0);
+}
