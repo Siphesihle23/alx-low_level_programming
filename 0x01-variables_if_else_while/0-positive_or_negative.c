299122f9485e58d2640b3f84e3cd901d7c50d5ca
Siphesihle <sihles406@gmail.com>
2023-02-17 11:37:24 -0800

    megetive positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n < 0)
-		printf("%d is negetive\n");
+		printf("%d is negetive\n",n);
 	if (n > 0)
-		printf("%d is positive\n");
+		printf("%d is positive\n",n);
 	if (n == 0)
-		printf("%d is zero\n");
+		printf("%d is zero\n",n);
 	return (0);
 }
