3cec265891636432d4add15627abf1cd03a0fdba
Siphesihle <sihles406@gmail.com>
2023-02-16 17:03:07 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line withe put function
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle")
+	return (0)
+}
