8a9d705149d6a0a2a611d812cae00c1e42b49f78
Siphesihle <sihles406@gmail.com>
2023-02-16 19:48:22 -0800

    uote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a c program that print a line
+ *
+ * Return: Always 1(Sucess)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1)
+}
