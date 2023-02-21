edc7d9913ea4bb793e0f269dd45a5becbd6daaec
Siphesihle <sihles406@gmail.com>
2023-02-21 10:26:55 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * print_alphabet - the program to print the alphebet
+ *
+ */
+void print_alphabet(void)
+{
+/*
+ * print_alphabet - the program to print the alphebet
+ *
+ */
+	char c;
+
+	putchar(c);
+	putchar("\n");
+}
+/*
+ * main - chech the code
+ *
+ * Return: always 0 sucess
+ */
+int main(void)
+{
+/*
+ * main - chech the code
+ *
+ * Return: always 0 sucess
+ */
+	print_alphabet(void);
+	return (0);
+}
