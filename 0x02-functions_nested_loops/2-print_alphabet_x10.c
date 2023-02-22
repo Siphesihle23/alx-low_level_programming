f5fde57926a015343d71f46b82ad27dd0bbade3e
Siphesihle <sihles406@gmail.com>
2023-02-22 05:36:43 -0800

    alphabet times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_alphabet - make alpabet
+ *print_alphabet_x10 - make alpabet
  *Return: void
  */
 void print_alphabet_x10(void)
@@ -14,6 +14,6 @@ void print_alphabet_x10(void)
 		{
 			_putchar(c);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
