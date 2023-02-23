2c26b7a05e6becb48d76d2769d1e080500818de4
Siphesihle <sihles406@gmail.com>
2023-02-22 17:52:16 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs -  print abdolur value of integer
+ * @c: the number to be computed
+ * Return: asolute value or zero
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
